d8a4ee93075bf630a872e12c3146f79109a215e5
Pau <paula.a.churquina@gmail.com>
2026-03-12 21:18:33 -0300

    adding github actions for cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "dii63j",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
