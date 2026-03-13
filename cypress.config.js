02298ba00528d2400071d20ea36c4f6cb0d2fd56
Pau <paula.a.churquina@gmail.com>
2026-03-12 21:28:53 -0300

    adding new video configuration for cypress run

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "dii63j",
+
+  //project config
+  video: false,
+  screenshotOnRunFailure: true,
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
